ca0d45fb44071c751c4408b79a39170876f76a06
Dhruv Godhani <dhruv.g@begenuin.com>
2026-06-20 12:36:23 +0530

    Refactor: Comment out projects section and update resume data structure

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -106,133 +106,133 @@ export const DATA = {
       end: "2021",
     },
   ],
-  projects: [
-    {
-      title: "Bike Setu 🛵",
-      href: "",
-      dates: "",
-      active: true,
-      description:
-        "I developed a web scraper that extracts student result data from the university site using Selenium automation. I also integrated a Gemini model using LangChain and created a FastAPI for text recognition",
-      technologies: [
-        "React.js",
-        "Three.js",
-        "Node.js",
-        "PostgreSQL",
-        "Python",
-        "FastAPI",
-        "Gemini",
-      ],
-      links: [],
-      image:
-        "https://res.cloudinary.com/dbmy60hnl/image/upload/v1757565959/ef80d72a-8db2-4eb1-9159-49a5ce976505.png",
-      video: "",
-    },
-    /* {
-      title: "Scholar Scraper 🎓",
-      href: "",
-      dates: "",
-      active: true,
-      description:
-        "I developed a web scraper that extracts student result data from the university site using Selenium automation. I also integrated a Gemini model using LangChain and created a FastAPI for text recognition",
-      technologies: ["Python", "FastAPI", "Gemini", "Selenium"],
-      links: [],
-      image:
-        "https://res.cloudinary.com/dbmy60hnl/image/upload/v1757566193/953fd874-7121-4eb0-95c4-dd7fa834d2d6.png",
-      video: "",
-    }, */
-    /*{
-      title: "ERP for College Management 🏫",
-      href: "",
-      dates: "",
-      active: true,
-      description:
-        "Developing an ERP system using the PERN stack with TypeScript. The system allows faculty to view their lecture schedules in a calendar view, mark student attendance, and download attendance reports in PDF format.  Creating a mobile app using Flutter, enabling students to view their attendance records",
-      technologies: [
-        "TypeScript",
-        "React.js",
-        "PostgreSQL",
-        "Node.js",
-        "Prisma",
-      ],
-      links: [],
-      image:
-        "https://res.cloudinary.com/dbmy60hnl/image/upload/v1757565935/eed4b03d-fd6c-4703-8fa9-d97ca087f868.png",
-      video: "",
-    }, */
-    {
-      title: "WiAttend - An Attendance App 🛜",
-      href: "",
-      dates: "",
-      active: true,
-      description:
-        "Developed a cross-platform application for colleges and universities, implementing a Smart WiFi-Based Attendance System.  The application incorporates three authentication layers, including device-native Face Recognition for enhanced security also Wifi Authentication layer",
-      technologies: ["Flutter", "Python", "Django", "MongoDB", "AWS"],
-      links: [],
-      image:
-        "https://res.cloudinary.com/dbmy60hnl/image/upload/v1757565981/b196b57a-4f7d-4822-9636-42d53d68f4d5.png",
-      video: "",
-    },
-    {
-      title: "Recruitment Radar",
-      href: "",
-      dates: "",
-      active: true,
-      description:
-        "I created a project to receive alerts when new companies post jobs on our Edunet Foundation job portal. I used Puppeteer to scrape recruitment data and set up cron jobs to send email alerts for new postings.",
-      technologies: ["TypeScript", "Next.js", "Puppeteer"],
-      links: [],
-      image:
-        "https://res.cloudinary.com/dbmy60hnl/image/upload/v1757565996/e78c5313-8cf8-45bf-b343-d4e7d30bdb12.png",
-      video: "",
-    },
-    {
-      title: "Slice Master 🍕",
-      href: "",
-      dates: "",
-      active: true,
-      description:
-        "Slice Master is a modern web application for restaurant management that enhances the dining experience with secure authentication, an interactive menu, customizable orders, and seamless Razorpay payments. It features an admin dashboard for managing items, ingredients, and orders, along with real-time tracking, live updates, and low-stock alerts to ensure smooth operations.",
-      technologies: [
-        "JavaScript",
-        "React.js",
-        "Node.js",
-        "Socket.io",
-        "MongoDB",
-        "Cloudinary",
-      ],
-      links: [],
-      image:
-        "https://res.cloudinary.com/dbmy60hnl/image/upload/v1757565987/54d6e707-38c4-4caf-a6df-7cadf1ac20ef.png",
-      video: "",
-    },
-    /* {
-      title: "Level up your fitness 💪",
-      href: "",
-      dates: "",
-      active: true,
-      description:
-        "It is a fitness app which helps you to track your daily fitness goal and guide you.",
-      technologies: ["JavaScript", "React.js", "Node.js", "MongoDB"],
-      links: [],
-      image:
-        "https://res.cloudinary.com/dbmy60hnl/image/upload/v1757566005/783f36ad-8fa1-4496-85c0-81921b2b98b5.png",
-      video: "",
-    },
-    {
-      title: "QR code Attendance System",
-      href: "",
-      dates: "",
-      active: true,
-      description:
-        "I created a college project for a Smart QR Code Attendance System, enabling students to mark their attendance simply by scanning their individual QR codes. To develop this project, I utilized OpenCV for real-time QR code detection from the camera feed, coupled with Python for decoding, and Node.js for the backend",
-      technologies: ["Python", "OpenCV", "Node.js", "MongoDB"],
-      links: [],
-      image:
-        "https://res.cloudinary.com/dbmy60hnl/image/upload/v1757566311/86507d2c-5aa2-4c6f-ab65-27bb7d11a250.png",
-      video: "",
-    }, */
-  ],
+//   projects: [
+//     {
+//       title: "Bike Setu 🛵",
+//       href: "",
+//       dates: "",
+//       active: true,
+//       description:
+//         "I developed a web scraper that extracts student result data from the university site using Selenium automation. I also integrated a Gemini model using LangChain and created a FastAPI for text recognition",
+//       technologies: [
+//         "React.js",
+//         "Three.js",
+//         "Node.js",
+//         "PostgreSQL",
+//         "Python",
+//         "FastAPI",
+//         "Gemini",
+//       ],
+//       links: [],
+//       image:
+//         "https://res.cloudinary.com/dbmy60hnl/image/upload/v1757565959/ef80d72a-8db2-4eb1-9159-49a5ce976505.png",
+//       video: "",
+//     },
+//     /* {
+//       title: "Scholar Scraper 🎓",
+//       href: "",
+//       dates: "",
+//       active: true,
+//       description:
+//         "I developed a web scraper that extracts student result data from the university site using Selenium automation. I also integrated a Gemini model using LangChain and created a FastAPI for text recognition",
+//       technologies: ["Python", "FastAPI", "Gemini", "Selenium"],
+//       links: [],
+//       image:
+//         "https://res.cloudinary.com/dbmy60hnl/image/upload/v1757566193/953fd874-7121-4eb0-95c4-dd7fa834d2d6.png",
+//       video: "",
+//     }, */
+//     /*{
+//       title: "ERP for College Management 🏫",
+//       href: "",
+//       dates: "",
+//       active: true,
+//       description:
+//         "Developing an ERP system using the PERN stack with TypeScript. The system allows faculty to view their lecture schedules in a calendar view, mark student attendance, and download attendance reports in PDF format.  Creating a mobile app using Flutter, enabling students to view their attendance records",
+//       technologies: [
+//         "TypeScript",
+//         "React.js",
+//         "PostgreSQL",
+//         "Node.js",
+//         "Prisma",
+//       ],
+//       links: [],
+//       image:
+//         "https://res.cloudinary.com/dbmy60hnl/image/upload/v1757565935/eed4b03d-fd6c-4703-8fa9-d97ca087f868.png",
+//       video: "",
+//     }, */
+//     {
+//       title: "WiAttend - An Attendance App 🛜",
+//       href: "",
+//       dates: "",
+//       active: true,
+//       description:
+//         "Developed a cross-platform application for colleges and universities, implementing a Smart WiFi-Based Attendance System.  The application incorporates three authentication layers, including device-native Face Recognition for enhanced security also Wifi Authentication layer",
+//       technologies: ["Flutter", "Python", "Django", "MongoDB", "AWS"],
+//       links: [],
+//       image:
+//         "https://res.cloudinary.com/dbmy60hnl/image/upload/v1757565981/b196b57a-4f7d-4822-9636-42d53d68f4d5.png",
+//       video: "",
+//     },
+//     {
+//       title: "Recruitment Radar",
+//       href: "",
+//       dates: "",
+//       active: true,
+//       description:
+//         "I created a project to receive alerts when new companies post jobs on our Edunet Foundation job portal. I used Puppeteer to scrape recruitment data and set up cron jobs to send email alerts for new postings.",
+//       technologies: ["TypeScript", "Next.js", "Puppeteer"],
+//       links: [],
+//       image:
+//         "https://res.cloudinary.com/dbmy60hnl/image/upload/v1757565996/e78c5313-8cf8-45bf-b343-d4e7d30bdb12.png",
+//       video: "",
+//     },
+//     {
+//       title: "Slice Master 🍕",
+//       href: "",
+//       dates: "",
+//       active: true,
+//       description:
+//         "Slice Master is a modern web application for restaurant management that enhances the dining experience with secure authentication, an interactive menu, customizable orders, and seamless Razorpay payments. It features an admin dashboard for managing items, ingredients, and orders, along with real-time tracking, live updates, and low-stock alerts to ensure smooth operations.",
+//       technologies: [
+//         "JavaScript",
+//         "React.js",
+//         "Node.js",
+//         "Socket.io",
+//         "MongoDB",
+//         "Cloudinary",
+//       ],
+//       links: [],
+//       image:
+//         "https://res.cloudinary.com/dbmy60hnl/image/upload/v1757565987/54d6e707-38c4-4caf-a6df-7cadf1ac20ef.png",
+//       video: "",
+//     },
+//     /* {
+//       title: "Level up your fitness 💪",
+//       href: "",
+//       dates: "",
+//       active: true,
+//       description:
+//         "It is a fitness app which helps you to track your daily fitness goal and guide you.",
+//       technologies: ["JavaScript", "React.js", "Node.js", "MongoDB"],
+//       links: [],
+//       image:
+//         "https://res.cloudinary.com/dbmy60hnl/image/upload/v1757566005/783f36ad-8fa1-4496-85c0-81921b2b98b5.png",
+//       video: "",
+//     },
+//     {
+//       title: "QR code Attendance System",
+//       href: "",
+//       dates: "",
+//       active: true,
+//       description:
+//         "I created a college project for a Smart QR Code Attendance System, enabling students to mark their attendance simply by scanning their individual QR codes. To develop this project, I utilized OpenCV for real-time QR code detection from the camera feed, coupled with Python for decoding, and Node.js for the backend",
+//       technologies: ["Python", "OpenCV", "Node.js", "MongoDB"],
+//       links: [],
+//       image:
+//         "https://res.cloudinary.com/dbmy60hnl/image/upload/v1757566311/86507d2c-5aa2-4c6f-ab65-27bb7d11a250.png",
+//       video: "",
+//     }, */
+//   ],
   hackathons: [
     {
       title: "Odoo x Amalthea",
